cf608cbc33699c3d16e422b32855091f222a603c
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-02-25 10:54:55 +0000

    job content with result div variable

--- scraper.py
+++ scraper.py
@@ -7,4 +7,5 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
 #all jobs cards | it holds div which contains every single job
-results = soup.find(id="ResultsContainer")
+# results = soup.find(id="ResultsContainer")
+# job_cards = results.find_all("div", class_ = "card-content")
