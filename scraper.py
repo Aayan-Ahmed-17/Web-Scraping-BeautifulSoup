ba746c657e1257dafc96c0274a72db254f368932
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-02-25 11:01:03 +0000

    loop through job_cards

--- scraper.py
+++ scraper.py
@@ -9,3 +9,5 @@ soup = BeautifulSoup(page.content, "html.parser")
 #all jobs cards | it holds div which contains every single job
 # results = soup.find(id="ResultsContainer")
 # job_cards = results.find_all("div", class_ = "card-content")
+# for job_card in job_cards:
+#     print(job_card, end="\n" * 2)
