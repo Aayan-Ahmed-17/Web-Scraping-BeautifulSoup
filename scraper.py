a1fde250f0b2e338f145665ca7745db4b43b1cbd
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-02-25 10:48:52 +0000

    find by id | extracted specific div which contains all jobs

--- scraper.py
+++ scraper.py
@@ -4,4 +4,7 @@ from bs4 import BeautifulSoup
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser").prettify()
+soup = BeautifulSoup(page.content, "html.parser")
+
+#all jobs cards | it holds div which contains every single job
+results = soup.find(id="ResultsContainer")
